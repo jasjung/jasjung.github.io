acc0ae72a8a87247bfc7ed972d1c7b1bad811434
Jason Jung <insikjung2017@u.northwestern.edu>
2020-04-02 19:59:06 -0700

    books update

--- assets/json/csv_to_json.py
+++ assets/json/csv_to_json.py
@@ -1,5 +1,9 @@
 import pandas as pd 
 
-df = pd.read_csv('books.csv')
+# df = pd.read_csv('books.csv')
+# df.to_json('books2.json',orient='records')
 
-df.to_json('books2.json',orient='records')
+df = pd.read_excel('books.xlsx')
+df.to_json('books.json',orient='records')
+print(df)
+
